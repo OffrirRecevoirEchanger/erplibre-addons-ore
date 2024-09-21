4a82d889301b3a2d07f695655eaeb48afd0b5446
Mathieu Benoit <mathben@technolibre.ca>
2024-09-21 12:08:43 -0400

    [ADD] ore_dev: transform database ready for developper

---
+++ ore_dev/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Change database for developper about ORE project",
+    "version": "12.0.1.0",
+    "author": "TechnoLibre",
+    "license": "AGPL-3",
+    "installable": True,
+    "post_init_hook": "post_init_hook",
+}
