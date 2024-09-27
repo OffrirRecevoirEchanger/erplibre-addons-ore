7a3b735c4a0728187eff99a3afad2ec71c57b816
Mathieu Benoit <mathben@technolibre.ca>
2024-09-26 22:08:06 -0400

    [FIX] website_chat_ore: migration script to force update data view

--- website_chat_ore/__manifest__.py
+++ website_chat_ore/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "ORE website chat",
     "category": "Website",
-    "version": "12.0.1.0",
+    "version": "12.0.2.0",
     "author": "TechnoLibre",
     "license": "AGPL-3",
     "website": "https://technolibre.ca",
